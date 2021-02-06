0e776784a619fb5091ed34eface72fe6d4155a4e
gurezo <akihiko.kigure@gmail.com>
2021-02-06 21:31:40 +0900

    smple add.

--- arduino.sh
+++ arduino.sh
@@ -10,7 +10,7 @@ wget https://downloads.arduino.cc/arduino-1.8.13-linuxaarch64.tar.xz
 cd /home/pi/
 mkdir /home/pi/Applications/
 if [ ! -d /home/pi/Applications/arduino-1.8.13/ ]; then
-    tar xvf arduino-1.8.13-linuxarm.tar.xz
+    tar xvf arduino-1.8.13-linuxaarch64.tar.xz
     mv arduino-1.8.13 /home/pi/Applications/
 fi
 cd /home/pi/Applications/
@@ -19,5 +19,5 @@ ln -s arduino-1.8.13 arduino
 # sudo ln -s arduino-1.8.13 arduino
 cd /home/pi/Applications/arduino/
 ./install.sh
-rm -f /home/pi/arduino-1.8.13-linuxarm.tar.xz
+rm -f /home/pi/arduino-1.8.13-linuxaarch64.tar.xz
 cd /home/pi/
