a78a8145bf76e2b17eabd894781f60a99d658990
gurezo <akihiko.kigure@gmail.com>
2021-02-07 02:34:04 +0900

    sh modified

--- arduino.sh
+++ arduino.sh
@@ -9,14 +9,15 @@ wget https://downloads.arduino.cc/arduino-1.8.13-linuxaarch64.tar.xz
 # cd /home/pi/
 # mkdir /home/pi/Applications/
 if [ ! -d /home/pi/Applications/arduino-1.8.13/ ]; then
+    mkdir /home/pi/Applications
     tar xvf arduino-1.8.13-linuxaarch64.tar.xz
-    mv arduino-1.8.13 /home/pi/Applications/arduino
+    mv arduino-1.8.13 /home/pi/Applications/
 fi
-cd /home/pi/Applications/
+# cd /home/pi/Applications/
 # FIXME: ln が失敗する
 # ln -s arduino-1.8.13 arduino
 # sudo ln -s arduino-1.8.13 arduino
-cd /home/pi/Applications/arduino/
+cd /home/pi/Applications/arduino-1.8.13/
 ./install.sh
 rm -f /home/pi/chirimen/arduino-1.8.13-linuxaarch64.tar.xz
 cd /home/pi/
