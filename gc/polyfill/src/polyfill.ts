b831abc043b21807b1d4f825f7234d1619fbd141
gurezo <akihiko.kigure@gmail.com>
2021-03-07 00:08:52 +0900

    porting ts file

--- gc/polyfill/src/polyfill.ts
+++ gc/polyfill/src/polyfill.ts
@@ -4,9 +4,11 @@ import { I2CAccess } from './I2CAccess';
 import { I2CPort } from './I2CPort';
 
 (() => {
-    const _GPIOAccess = new GPIOAccess(); 
-    const _GPIOPort = new GPIOPort(); 
-    const _I2CAccess = new I2CAccess(); 
-    const _I2CPort = new I2CPort(); 
+  const serverURL = "wss://localhost:33330/";
+
+  const _GPIOAccess = new GPIOAccess(); 
+  const _GPIOPort = new GPIOPort(); 
+  const _I2CAccess = new I2CAccess(); 
+  const _I2CPort = new I2CPort(); 
 
 })();
